105f029e1cf9bbb24d51fe0906518114b3e9a3e5
betriz-sanchez-flores <beatrizsanchezflores2000@gmail.com>
2021-02-17 23:16:16 -0600

    Ejemplo9

---
+++ Ejemplo9/js1.js
@@ -0,0 +1,6 @@
+//Convertir a string
+foo.toString();
+//Convertir string a entero
+Number.parseInt('55'); // 55 como un numero
+//Convertir string a number
+Number.parseFloat("55.99");//55.99 como numero
